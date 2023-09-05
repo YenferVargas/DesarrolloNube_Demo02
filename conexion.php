f22dbdf848c0bce9e4cb67323cb8ffdad5b42510
yenif <yenifer.vargas@tecsup.edu.pe>
2023-09-05 10:21:11 -0500

    "agrgenado"

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
 	function conexion(){
 		$host = "host=containers-us-west-112.railway.app";
-		$port = "port=6969";
+		$port = "6969";
 		$dbname = "dbname=railway";
 		$user = "user=postgres";
 		$password = "password=G6EwWHhXWpuP0oCHAgXq";
