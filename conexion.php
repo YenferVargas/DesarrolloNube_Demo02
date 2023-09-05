78ee2ed09d60b4ed346dd41c2ec0b4a73e1db87a
yenif <yenifer.vargas@tecsup.edu.pe>
2023-09-05 10:17:53 -0500

    "cambio"

--- conexion.php
+++ conexion.php
@@ -1,15 +1,15 @@
 <?php
 
 	function conexion(){
+		$host = "host=containers-us-west-112.railway.app";
+		$port = "port=6969";
+		$dbname = "dbname=railway";
+		$user = "user=postgres";
+		$password = "password=G6EwWHhXWpuP0oCHAgXq";
 
-	$host = "host=containers-us-west-112.railway.app";
-	$port = "port=6969";
-	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "password=G6EwWHhXWpuP0oCHAgXq";
+		$db = pg_connect("$host $port $dbname $user $password");
 
-	$db = pg_connect("$host $port $dbname $user $password");
+		return $db;
+	}
 
-	return $db;
-}
 ?>
